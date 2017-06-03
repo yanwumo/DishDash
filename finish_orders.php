c9ce608093100b561cee7e50cc8272f8bd7fe9dc
Ben Yan <yanwumo@outlook.com>
2017-06-03 10:49:17 +0800

    Add interface

---
+++ finish_orders.php
@@ -0,0 +1,34 @@
+<?php
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    exit();
+}
+$ids = $_GET['id'];
+$db = new PDO('sqlite:data.sqlite');
+
+foreach ($ids as $id) {
+    $stmt = $db->prepare("SELECT table_id FROM orders WHERE id = :id");
+    $stmt->bindParam(":id", $id);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (isset($table) && $table != $row['table_id']) {
+        echo "error";
+        exit();
+    }
+    $table = $row['table_id'];
+}
+
+$stmt = $db->prepare("SELECT distance FROM tables WHERE id = :id");
+$stmt->bindParam(":id", $table);
+$stmt->execute();
+if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo "success " . $row['distance'];
+} else {
+    echo "error";
+    exit();
+}
+
+foreach ($ids as $id) {
+    $stmt = $db->prepare("DELETE FROM orders WHERE id = :id");
+    $stmt->bindParam(":id", $id);
+    $stmt->execute();
+}
